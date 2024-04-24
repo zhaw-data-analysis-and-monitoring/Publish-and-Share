0a8830ad865b4588ed0136150cf271b6b7621724
ratnanil <rata@zhaw.ch>
2024-04-24 11:58:45 +0200

    rm GOT data and script

--- got-0.R
+++
@@ -1,9 +0,0 @@
-screentimes <- read_csv("data/GOT_screentimes.csv")
-
-
-screentimes <- screentimes |> 
-  mutate(
-    episodes = as.integer(episodes)
-  )
-
-write_csv(screentimes, "data/GOT_screentimes_1.csv")
